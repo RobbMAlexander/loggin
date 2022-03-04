d639e367d536fa78a39222c1c7eeff20b08cc6fd
Robb Alexander <robbmalexander@gmail.com>
2022-03-03 17:26:37 -0800

    initial lab 401.14 setup

---
+++ src/main/java/com/robbmalexander/loggin/LogginApplication.java
@@ -0,0 +1,13 @@
+package com.robbmalexander.loggin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogginApplication.class, args);
+	}
+
+}
